89e57d44eeb599b72f2fbd9e7ce5b433160df540
J/A <archer884@gmail.com>
2015-03-08 04:56:52 -0400

    Chuck Norris can judge a book by its cover.

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+#![feature(io, plugin)]
+#![plugin(regex_macros)]
+
+extern crate hyper;
+extern crate regex;
+
+use hyper::Client;
+use std::io::Read;
+
+fn main() {
+    let content_pattern = regex!("\"[A-Z][^\"]+\"");
+    let mut client = Client::new();
+
+    let mut res = match client.get("http://api.icndb.com/jokes/random").send() {
+        Ok(res) => res,
+        _ => {
+            println!("Funny not found.");
+            return;
+        }
+    };
+
+    let body = {
+        let mut body = String::new();
+        res.read_to_string(&mut body).unwrap();
+        body
+    };
+
+    let x = content_pattern
+        .captures(&body).unwrap()
+        .at(0).unwrap();
+
+    println!("{}", &x[1..x.len() - 1]);
+}
